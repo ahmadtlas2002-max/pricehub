ef6940925ef5e33b4fdaabd26b10970ad7e4b815
ahmadtlas2002-max <ahmadtlas2002@gmail.com>
2026-06-19 05:14:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = "pricehub-v1";
+const CACHE_NAME = "pricehub-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./firebase.js"
-      ]);
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
